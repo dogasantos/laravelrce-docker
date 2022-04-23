296f6c42b53cb41a0d5fc9b9256644a8a26cab48
Douglas S. Santos <douglas.secco@gmail.com>
2022-04-23 14:02:29 -0300

    Update docker-setup.sh

--- docker-setup.sh
+++ docker-setup.sh
@@ -8,6 +8,8 @@ apt install -y php-xml php-mbstring
 
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
+cd /var/www && rm -rf html && composer create-project laravel/laravel . "v8.4.2" && sed -i -E 's|"facade/ignition": ".+?"|"facade/ignition": "2.5.1"|g' composer.json && composer update && mv public html
+
 sed -i -E 's|index index|index index.php index|g' /etc/nginx/sites-enabled/default
 sed -i -E 's|try_files \$uri.*|try_files \$uri \$uri/ /index.php?\$query_string;|g' /etc/nginx/sites-enabled/default
 sed -i -E 's|#location ~ \\\.php\$ \{|location ~ \\.php\$ {\n\t\tinclude snippets/fastcgi-php.conf;\n\t\tfastcgi_pass 127.0.0.1:9000;\n\t}|g' /etc/nginx/sites-enabled/default
